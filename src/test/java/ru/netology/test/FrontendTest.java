26342084988908cca0d0b7b1bf0f9fba2d1081ae
Alexander Pavlenko <pava@bk.ru>
2020-07-24 16:33:02 +0700

    Allure Reports Added

--- src/test/java/ru/netology/test/FrontendTest.java
+++ src/test/java/ru/netology/test/FrontendTest.java
@@ -1,7 +1,10 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import lombok.val;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DbHelper;
@@ -12,6 +15,16 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class FrontendTest {
     private LoginPage loginPage;
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     public void openPage() {
